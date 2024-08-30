54023f0949a71459e56fe67f50dab0b637a4b7f2
Himanshu Chaurasiya <137139619+himanshu9997@users.noreply.github.com>
2024-08-30 09:56:43 +0530

    add-digits

--- 258-add-digits/add-digits.java
+++ 258-add-digits/add-digits.java
@@ -2,7 +2,7 @@ class Solution {
     public int addDigits(int num) {
         int value = num;
         while(value > 9){
-            int newValue =0,rem;
+            int newValue =0,rem; 
             while( value != 0){
                 rem = value%10;
                 newValue+=rem;
