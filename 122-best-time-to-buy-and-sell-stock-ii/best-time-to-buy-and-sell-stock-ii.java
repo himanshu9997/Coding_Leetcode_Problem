220d7168bff75ba1f79685f3517e6e76b7f7d0c1
Himanshu Chaurasiya <137139619+himanshu9997@users.noreply.github.com>
2024-09-14 10:36:27 +0530

    Time: 1 ms (90.31%) | Memory: 45.7 MB (45.34%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -12,10 +12,5 @@ public class Solution {
         return maxProfit;
     }
 
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        int[] prices = {7, 1, 5, 3, 6, 4};
-        int result = solution.maxProfit(prices);
-        System.out.println(result);
-    }
+   
 }
