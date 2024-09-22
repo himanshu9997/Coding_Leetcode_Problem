a69e888dd79339e08b4157c77755ca0a40af605a
Himanshu Chaurasiya <137139619+himanshu9997@users.noreply.github.com>
2024-09-22 08:58:37 +0530

    Time: 4 ms (73.47%) | Memory: 42.8 MB (90.90%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,8 +1,5 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        // char[] sChar = s.toCharArray();
-        // char[] tChar = t.toCharArray();
-
         int charA[] = new int[26]; 
 
         for(int i = 0;i < s.length();i++) {
@@ -20,9 +17,6 @@ class Solution {
             }
         }
         return true;
-        // Arrays.sort(sChar);
-        // Arrays.sort(tChar);
 
-        // return Arrays.equals(sChar, tChar);
     }
 }
